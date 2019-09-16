fe42e4b2924575c39f56515fff1f657a825ba51f
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-16 15:04:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,12 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+require 'pry'
 
 def key_for_min_value(name_hash)
-
+  values = []
+  name = ""
+  name_hash.collect do |key, value|
+    values << value
+    values.max_by {|a, b| a <=> b}
+    binding.pry
+  end
+  name
 end

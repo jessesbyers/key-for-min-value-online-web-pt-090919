1a3ef28c3c7ceb8fee693bfe765d027de672ae8e
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-16 16:00:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,8 @@ def key_for_min_value(name_hash)
     values << value
   if name_hash.value?(values.minmax[0])
     name = name_hash.index(values.minmax[0])
-  else
-    return "nil"
+#  else
+#    return "nil"
 end
   end
   name

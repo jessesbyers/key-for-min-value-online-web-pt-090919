ef387ac0f61d85cd935fdf273312bb6046b55ba8
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-16 15:29:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@ require 'pry'
 def key_for_min_value(name_hash)
   values = []
   name = ""
-  name_hash.each do |key, value|
+  name_hash.collect do |key, value|
     values << value
     binding.pry
 

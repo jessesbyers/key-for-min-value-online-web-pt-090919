0c5e8d55bca3b773140fa233c88be5cb622a678b
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-16 16:03:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,19 +1,9 @@
-require 'pry'
-
 def key_for_min_value(name_hash)
   values = []
   name_hash.collect do |key, value|
     values << value
   end
   if name_hash.value?(values.minmax[0])
-#    name = ""
     name = name_hash.index(values.minmax[0])
-#    name
-#  else
-#    return "nil"
-
   end
-
 end
-
-#value.max_by {|a, b| a <=> b}

78eea8150640708777aedc0ec2c32f129d1f301c
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-16 15:55:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   values = []
+  name = ""
   name_hash.collect do |key, value|
     values << value
   if name_hash.value?(values.minmax[0])
     name = name_hash.index(values.minmax[0])
-    name
 end
   end
   name
